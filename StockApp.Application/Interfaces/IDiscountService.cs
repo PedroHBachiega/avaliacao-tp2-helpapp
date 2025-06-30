3ec3a3339a32199dbd64f7ce2cb4b3f53a95970a
VitorBZS <bonilhazanattasilva@gmail.com>
2025-06-29 22:09:38 -0300

    Criado Sistema de Descontos e Promoções

---
+++ StockApp.Application/Interfaces/IDiscountService.cs
@@ -0,0 +1,8 @@
+using System;
+namespace StockApp.Application.Interfaces
+{
+    public interface IDiscountService
+    {
+        decimal ApplyDiscount(decimal price, decimal? discountPercentage);
+    }
+}
